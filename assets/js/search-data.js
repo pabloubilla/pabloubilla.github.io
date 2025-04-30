77752021c3ef889a50959fbb8f6feeefa9949bb7
pabloubilla <48734361+pabloubilla@users.noreply.github.com>
2025-04-30 22:42:59 +0000

    Deploying to gh-pages from @ pabloubilla/pabloubilla.github.io@0f9940c390568ac8e440744ce4742b62defe081e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Materials for courses you taught. Replace this text with your description. This is more description",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
